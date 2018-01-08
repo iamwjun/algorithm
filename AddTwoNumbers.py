49e3c150a66c4aed23ee33954d6127b34a7cc810
iamk <cddxwujun@qq.com>
2018-01-08 22:01:18 +0800

    add tow numbers

---
+++ AddTwoNumbers.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+one = ListNode(0)
+cur = one
+node = ListNode(add)
+
+
+
+
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        print(l1(0))
+        node = l2.head
+        while node:
+            print(node.value)
+            node = node.next
+        for node in l1:
+            print(node.value)
+
+Solution().addTwoNumbers(one, two)
