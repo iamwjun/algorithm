0a0edd1ca51e418540a976176995b5d1cb48c57e
吴俊 <wuj@eeparking.com>
2018-01-11 17:34:10 +0800

    方法2

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -3,32 +3,78 @@ from operator import is_not
 from functools import partial
 from itertools import combinations, permutations
 
-class Solution:
+# 方法1，数据大时运行速度慢
+
+# class Solution:
+#     def lengthOfLongestSubstring(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         retype = 0
+#         re = []
+#         for c in s:
+#             if c not in re:
+#                 re.append(c)
+#         i = len(''.join(re))
+#         l = ''.join(re)
+#         if i == len(s):
+#             return i
+#         while i > 0:
+#         # f = open('log.txt','w')
+#             for w in list(permutations(l, 8)):
+#                 # f.write("string:{0} bool:{1}\n".format(''.join(w), ''.join(w) in s))
+#                 # print("string:{0} bool:{1}".format(''.join(w), ''.join(w) in s))
+#                 if ''.join(w) in s:
+#                     print(''.join(w))
+#                     retype = len(''.join(w))
+#             if retype > 0:
+#                 break
+#             i = i - 1
+#         # f.close()
+
+#         return retype
+
+# 方法2
+
+class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        retype = 0
-        re = []
-        for c in s:
-            if c not in re:
-                re.append(c)
-        i = len(''.join(re))
-        l = ''.join(re)
-        if i == len(s):
-            return i
-        while i > 0:
-            for w in list(permutations(l, i)):
-                # print("string:{0} bool:{1}".format(''.join(w), ''.join(w) in s))
-                if ''.join(w) in s:
-                    retype = len(''.join(w))
-            if retype > 0:
-                break
-            i = i - 1
+        longest = 0
+        substring_map = {}
+        start = 0
+        for i in range(len(s)):
+            print(i)
+            position = substring_map.get(s[i])
+            if position is not None and position >= start:                 
+                length = i - start
+                start = position + 1                
+                longest = max(length, longest)
+            substring_map[s[i]] = i
+            print(substring_map)
 
-        return retype
+        longest = max(len(s) - start, longest)
+        return longest
 
+# class Solution(object):
+#     def lengthOfLongestSubstring(self, s):
+#         """
+#         :type s: str
+#         :a
+#         :c: int 最长长度
+#         """
+#         position = 0
+#         substring_map = {}
+#         start = 0
+#         for i in range(len(s)):
+#             position = substring_map.get(s[i])
+#             if position is not None and position >= start:
+#                 length = 
+        
+#         return longest
 
 if __name__ == '__main__':
     str = "xrstenkqqpj"
